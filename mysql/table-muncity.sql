3b28a801c43cc6e87c8bcb8b9c06968c2237dfb7
Joeylene Rivera <joeylenerivera@gmail.com>
2018-05-05 13:28:30 +0800

    Added id attribute

--- mysql/table-muncity.sql
+++ mysql/table-muncity.sql
@@ -19,32 +19,32 @@ CREATE TABLE `muncity` (
 -- ------------------------------------
 
 -- Data Total: 1489 municipalities + 145 cities = 1634
-INSERT INTO `muncity` (`province_id`,`district`,`code`,`description`)
+INSERT INTO `muncity` (`muncity_id`,`province_id`,`district`,`code`,`description`)
 VALUES
 -- Ilocos Norte PROVINCE ID : 1
-(1,'1st','012801000','Adams'),
-(1,'1st','012802000','Bacarra'),
-(1,'2nd','012803000','Badoc'),
-(1,'1st','012804000','Bangui'),
-(1,'2nd','012805000','City of Batac'),
-(1,'1st','012806000','Burgos'),
-(1,'1st','012807000','Carasi'),
-(1,'2nd','012808000','Currimao'),
-(1,'2nd','012809000','Dingras'),
-(1,'1st','012810000','Dumalneg'),
-(1,'2nd','012811000','Banna (espiritu)'),
-(1,'1st','012812000','Laoag City (capital)'),
-(1,'2nd','012813000','Marcos'),
-(1,'2nd','012814000','Nueva Era'),
-(1,'1st','012815000','Pagudpud'),
-(1,'2nd','012816000','Paoay'),
-(1,'1st','012817000','Pasuquin'),
-(1,'1st','012818000','Piddig'),
-(1,'2nd','012819000','Pinili'),
-(1,'2nd','012820000','San Nicolas'),
-(1,'1st','012821000','Sarrat'),
-(1,'2nd','012822000','Solsona'),
-(1,'1st','012823000','Vintar'),
+(1,1,'1st','012801000','Adams'),
+(2,1,'1st','012802000','Bacarra'),
+(3,1,'2nd','012803000','Badoc'),
+(4,1,'1st','012804000','Bangui'),
+(5,1,'2nd','012805000','City of Batac'),
+(6,1,'1st','012806000','Burgos'),
+(7,1,'1st','012807000','Carasi'),
+(8,1,'2nd','012808000','Currimao'),
+(9,1,'2nd','012809000','Dingras'),
+(10,1,'1st','012810000','Dumalneg'),
+(11,1,'2nd','012811000','Banna (espiritu)'),
+(12,1,'1st','012812000','Laoag City (capital)'),
+(13,1,'2nd','012813000','Marcos'),
+(14,1,'2nd','012814000','Nueva Era'),
+(15,1,'1st','012815000','Pagudpud'),
+(16,1,'2nd','012816000','Paoay'),
+(17,1,'1st','012817000','Pasuquin'),
+(18,1,'1st','012818000','Piddig'),
+(19,1,'2nd','012819000','Pinili'),
+(20,1,'2nd','012820000','San Nicolas'),
+(21,1,'1st','012821000','Sarrat'),
+(22,1,'2nd','012822000','Solsona'),
+(23,1,'1st','012823000','Vintar'),
 -- Ilocos Sur PROVINCE ID : 2
 
 -- NCR, CITY OF MANILA, FIRST DISTRICT (Not a Province) ID : ?
--- mysql/table-province.sql
+++ mysql/table-province.sql
@@ -18,27 +18,27 @@ CREATE TABLE `province` (
 -- ------------------------------------
 
 -- Data Total: 81
-INSERT INTO `province` (`region_id`,`code`,`description`)
+INSERT INTO `province` (`province_id`,`region_id`,`code`,`description`)
 VALUES
 -- Region I (Ilocos Region) REGION ID : 1
-(1,'012800000','Ilocos Norte'),
-(1,'012900000','Ilocos Sur'),
-(1,'013300000','La Union'),
-(1,'015500000','Pangasinan'),
+(1,1,'012800000','Ilocos Norte'),
+(2,1,'012900000','Ilocos Sur'),
+(3,1,'013300000','La Union'),
+(4,1,'015500000','Pangasinan'),
 -- Region II (Cagayan Valley) REGION ID : 2
-(2,'020900000','Batanes'),
-(2,'021500000','Cagayan'),
-(2,'023100000','Isabela'),
-(2,'025000000','Nueva Vizcaya'),
-(2,'025700000','Quirino'),
+(5,2,'020900000','Batanes'),
+(6,2,'021500000','Cagayan'),
+(7,2,'023100000','Isabela'),
+(8,2,'025000000','Nueva Vizcaya'),
+(9,2,'025700000','Quirino'),
 -- Region III (Central Luzon) REGION ID : 3
 
 
 -- National Capital Region (NCR) ID : 13
-(13,'133900000','NCR, CITY OF MANILA, FIRST DISTRICT (Not a Province)'),
-(13,'137600000','NCR, SECOND DISTRICT (Not a Province)'),
-(13,'137400000','NCR, THIRD DISTRICT (Not a Province)'),
-(13,'137500000','NCR, FOURTH DISTRICT (Not a Province)'),
+(?,13,'133900000','NCR, CITY OF MANILA, FIRST DISTRICT (Not a Province)'),
+(?,13,'137600000','NCR, SECOND DISTRICT (Not a Province)'),
+(?,13,'137400000','NCR, THIRD DISTRICT (Not a Province)'),
+(?,13,'137500000','NCR, FOURTH DISTRICT (Not a Province)'),
 -- CORDILLERA ADMINISTRATIVE REGION (CAR) ID : 14
-(14,'140100000','Abra'),
-(14,'148100000','Apayao');
+(?,14,'140100000','Abra'),
+(?,14,'148100000','Apayao');
